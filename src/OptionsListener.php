0f8033dad41236f996b4c91cf87a593b8536d90d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:12:48 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/OptionsListener.php
+++ src/OptionsListener.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas-api-tools/api-tools-rpc for the canonical source repository
- * @copyright https://github.com/laminas-api-tools/api-tools-rpc/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas-api-tools/api-tools-rpc/blob/master/LICENSE.md New BSD License
- */
-
 namespace Laminas\ApiTools\Rpc;
 
 use Laminas\EventManager\EventManagerInterface;
